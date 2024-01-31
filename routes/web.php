3bf67cc5c473da4177fe945cc3e3c00fb6fea212
nguyen van loi <nguyenvanloihd88@gmail.com>
2024-02-01 01:04:02 +0700

    layout

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layout');
+});
+Route::get('/room', function () {
+    return view('room.room_page');
+});
+Route::get('/news', function () {
+    return view('news.news_page');
 });
